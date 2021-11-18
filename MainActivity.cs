953045fb0008383d2137a26cff3c11b5867dd641
Arseniy Zorin <arsenchozzz@hotmail.com>
2021-11-18 18:15:37 +0300

    Add project files.

---
+++ MainActivity.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace ToolBar
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/MyTheme", MainLauncher = true)]
+    public class MainActivity : Activity //AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+
+            //Toolbar will now take on default actionbar characteristics
+            SetActionBar(toolbar);
+
+            ActionBar.Title = "Hello from Toolbar";
+
+
+            var toolbarBottom = FindViewById<Toolbar>(Resource.Id.toolbar_bottom);
+
+            toolbarBottom.Title = "Photo Editing";
+            toolbarBottom.InflateMenu(Resource.Menu.photo_edit);
+            toolbarBottom.MenuItemClick += (sender, e) => {
+                Toast.MakeText(this, "Bottom toolbar pressed: " + e.Item.TitleFormatted, ToastLength.Short).Show();
+            };
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.home, menu);
+            return base.OnCreateOptionsMenu(menu);
+        }
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            Toast.MakeText(this, "Top ActionBar pressed: " + item.TitleFormatted, ToastLength.Short).Show();
+            return base.OnOptionsItemSelected(item);
+        }
+    }
+}
